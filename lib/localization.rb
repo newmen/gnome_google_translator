b9d46fb541f4315fb39a8eb8c95926141f4f806e
Gleb Averchuk <altermn@gmail.com>
2014-07-13 15:02:34 +0400

    fix I18n deprecation message

--- lib/localization.rb
+++ lib/localization.rb
@@ -12,6 +12,7 @@ class L18ze
   def self.init(locale)
     unless @locale_files_list
       @locale_files_list = Dir[File.dirname(__FILE__) + '/' + PATH_TO_LOCALES + '/*.yml']
+      I18n.enforce_available_locales = true
       I18n.load_path << @locale_files_list
     end
     locales_list = @locale_files_list.map { |file_name| File.basename(file_name, '.yml') }
